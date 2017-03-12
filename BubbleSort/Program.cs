11d03c5878fc274e0ce23a58fdc48615dd8c1b0c
bintiM <marc.bintinger@yahoo.com>
2017-03-12 08:46:47 +0100

    Kommentare
    Sortierung dokumentiert

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -13,9 +13,18 @@ namespace BubbleSort
 
             int[] arr = { 5, 9, 1, 4, 6, 7, 3, 2, 8 };
 
+            Console.WriteLine("unsortiertes Array:");
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+    
+
+
             int[] sorted = bubble(arr);
 
-            for(int i = 0; i < sorted.Length; i++)
+            Console.WriteLine("\nsortiertes Array:");
+
+            for (int i = 0; i < sorted.Length; i++)
                 Console.Write(sorted[i] + " ");
 
             Console.ReadKey();
@@ -26,15 +35,18 @@ namespace BubbleSort
         {
             int temp = 0;
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array.Length; i++) // das Array einmal durchgehen
             {
-                for (int j = 0; j < array.Length - 1 - i; j++)
+                /*restliches unsortiertes Array durchsuchen, die größten Werte bubbeln nach rechts, der größte Wert landet nach der ersten Iteration ganz hinten, daher muss nicht mehr das ganze Array durchgegangen werden (array.Length - 1)
+                 * */
+                for (int j = 0; j < array.Length - 1 - i; j++) 
+                    
                 {
-                    if (array[j] > array[j + 1])
+                    if (array[j] > array[j + 1]) // wenn die linke Position größer ist als rechts, dann muss getauscht werden
                     {
-                        temp = array[j + 1];
-                        array[j + 1] = array[j];
-                        array[j] = temp;
+                        temp = array[j + 1]; // rechte Zahl merken
+                        array[j + 1] = array[j]; // linke Zahl nach rechts schieben
+                        array[j] = temp; // gemerkte Zahl nach links setzen
                     }
                 }
             }
